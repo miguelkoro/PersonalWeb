c1c22576cd956083e0113d8378cb56628edc257c
Miguelkoro <miguelkoro@gmail.com>
2025-07-15 23:25:58 +0200

    añadidas redes sociales al navbar

--- PersonalWeb/src/components/NavBar.jsx
+++ PersonalWeb/src/components/NavBar.jsx
@@ -26,29 +26,46 @@ const NavBar = () => {
           <p className="navbar-text">Miguelkoro</p>
         </div>
       </div>
-      <div className="navbar-manage">
-        <a href="#inicio" className="manage-title">{t('i.home')}</a>
-        <a href="#sobre-mi" className="manage-title">{t('i.aboutme')}</a>
-        <a href="#habilidades" className="manage-title">{t('i.skills')}</a>
-        <a href="#proyectos" className="manage-title">{t('i.projects')}</a>
-        <a href="#contacto" className="manage-title">{t('i.contact')}</a>
+      
+      <div className="navbar-center">
+        <div className="navbar-manage">
+          <a href="#inicio" className="manage-title">{t('i.home')}</a>
+          <a href="#sobre-mi" className="manage-title">{t('i.aboutme')}</a>
+          <a href="#habilidades" className="manage-title">{t('i.skills')}</a>
+          <a href="#proyectos" className="manage-title">{t('i.projects')}</a>
+          <a href="#contacto" className="manage-title">{t('i.contact')}</a>
+        </div>
       </div>
-      <div className="navbar-language"
-           onMouseEnter={handleLanguageMouseEnter}
-           onMouseLeave={handleLanguageMouseLeave}>
-        <span className="language-selector"><img src={language === 'es' ? esSvg : enSvg} alt="Language" className="language-icon" /> {language.toUpperCase()}</span>
-        {languageMenuOpen && (
-          <div className="dropdown-menu-language">
-            <div className={`dropdown-item-language ${language === 'es' ? 'active' : ''}`}
-              onClick={() => changeLanguage('es')}>
-              <img src={esSvg} alt="Español" className="language-icon" /> Español
+
+      <div className="navbar-right">
+        <div className="navbar-social">
+          <a href="https://github.com/miguelkoro" target="_blank" rel="noopener noreferrer" className="social-link github">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://linkedin.com/in/miguelkoro" target="_blank" rel="noopener noreferrer" className="social-link linkedin">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://twitter.com/miguelkoro" target="_blank" rel="noopener noreferrer" className="social-link twitter">
+            <i className="fab fa-twitter"></i>
+          </a>
+        </div>
+        <div className="navbar-language"
+             onMouseEnter={handleLanguageMouseEnter}
+             onMouseLeave={handleLanguageMouseLeave}>
+          <span className="language-selector"><img src={language === 'es' ? esSvg : enSvg} alt="Language" className="language-icon" /> {language.toUpperCase()}</span>
+          {languageMenuOpen && (
+            <div className="dropdown-menu-language">
+              <div className={`dropdown-item-language ${language === 'es' ? 'active' : ''}`}
+                onClick={() => changeLanguage('es')}>
+                <img src={esSvg} alt="Español" className="language-icon" /> Español
+              </div>
+              <div className={`dropdown-item-language ${language === 'en' ? 'active' : ''}`}
+                onClick={() => changeLanguage('en')}>
+                <img src={enSvg} alt="English" className="language-icon" /> English
+              </div>
             </div>
-            <div className={`dropdown-item-language ${language === 'en' ? 'active' : ''}`}
-              onClick={() => changeLanguage('en')}>
-              <img src={enSvg} alt="English" className="language-icon" /> English
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
 
     </nav>
