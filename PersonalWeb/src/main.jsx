31f4db5cc9de5b84374def317ed9da3f09958fc2
Miguelkoro_PC\Miguelkoro <miguelkoro@gmail.com>
2026-09-09 00:16:13 +0200

    Posible arreglo de las rutas

--- PersonalWeb/src/main.jsx
+++ PersonalWeb/src/main.jsx
@@ -5,6 +5,12 @@ import App from './App.jsx'
 import { DataProvider } from './context/DataContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+//Porque me sale error al actualizar una pagina con una ruta que no es la principal, y no me deja entrar a la pagina, entonces hago esto para que si hay un parametro p en la url, lo use como ruta de fallback.
+const fallbackRoute = new URLSearchParams(window.location.search).get('p');
+if (fallbackRoute) {
+  window.history.replaceState({}, '', decodeURIComponent(fallbackRoute));
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
